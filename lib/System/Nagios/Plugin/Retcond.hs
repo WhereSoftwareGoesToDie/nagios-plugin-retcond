f20c4be549c8c6778c6c23c42f921f847e5111cf
Sharif Olorin <sio@tesser.org>
2015-05-26 07:11:22 +0000

    Some ToPerfData instances

--- lib/System/Nagios/Plugin/Retcond.hs
+++ lib/System/Nagios/Plugin/Retcond.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module System.Nagios.Plugin.Retcond where
 
 import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Text (Text)
 import Data.Text.Lens
-import Control.Lens
+import Data.Int
+import Control.Lens hiding ((.=))
 import Control.Applicative
 import Control.Monad.IO.Class
 import Data.Aeson
@@ -19,24 +22,59 @@ data CheckOpts = CheckOpts
   { checkEkgEndpoint :: String }
 
 data DataSourceMeters = DataSourceMeters
-  { sourceNumNotifications :: Integer
-  , sourceNumKeys          :: Integer
-  }
+  { _sourceNumNotifications :: Int64
+  , _sourceNumKeys          :: Int64
+  , _sourceName             :: Text
+  } deriving (Eq, Show)
+
+makeLenses ''DataSourceMeters
 
 data EntityMeters = EntityMeters
-  { entityNumNotifications :: Integer
-  , entityNumCreates       :: Integer
-  , entityNumUpdates       :: Integer
-  , entityNumDeletes       :: Integer
-  , entityNumConflicts     :: Integer
-  , entityNumKeys          :: Integer
-  , entityDataSourceMeters :: Map Text DataSourceMeters
-  }
+  { _entityNumNotifications :: Int64
+  , _entityNumCreates       :: Int64
+  , _entityNumUpdates       :: Int64
+  , _entityNumDeletes       :: Int64
+  , _entityNumConflicts     :: Int64
+  , _entityNumKeys          :: Int64
+  , _entityDataSourceMeters :: [DataSourceMeters]
+  , _entityName             :: Text
+  } deriving (Eq, Show)
+
+makeLenses ''EntityMeters
+
+instance ToPerfData EntityMeters where
+    toPerfData em = let prefix = "entity_" <> (em ^. entityName) <> "_" in
+        [ barePerfDatum (prefix <> "notifications")
+                        (IntegralValue (em ^. entityNumNotifications))
+                        NullUnit
+        , barePerfDatum (prefix <> "creates")
+                        (IntegralValue (em ^. entityNumCreates))
+                        Counter
+        , barePerfDatum (prefix <> "updates")
+                        (IntegralValue (em ^. entityNumUpdates))
+                        Counter
+        , barePerfDatum (prefix <> "deletes")
+                        (IntegralValue (em ^. entityNumDeletes))
+                        Counter
+        , barePerfDatum (prefix <> "conflicts")
+                        (IntegralValue (em ^. entityNumConflicts))
+                        Counter
+        , barePerfDatum (prefix <> "keys")
+                        (IntegralValue (em ^. entityNumKeys))
+                        NullUnit
+        ]
 
 data RetconMeters = RetconMeters
-  { entityMeters           :: Map Text EntityMeters
-  , serverNumNotifications :: Integer
-  }
+  { _entityMeters           :: [EntityMeters]
+  , _serverNumNotifications :: Int64
+  } deriving (Eq, Show)
+
+makeLenses ''RetconMeters
+
+instance FromJSON RetconMeters where
+    parseJSON (Object o) = RetconMeters <$> return []
+                                        <*> o .: "gauge_notifications"
+    parseJSON _          = fail "RetconMeters must be an object"
 
 checkOptParser :: ParserInfo CheckOpts
 checkOptParser =  info (helper <*> opts)
@@ -59,7 +97,9 @@ checkRetcond = do
     let reqOpts = header "Accept" .~ ["application/json"] $ defaults
     resp <- liftIO $ getWith reqOpts checkEkgEndpoint
     case resp ^. responseStatus . statusCode of
-         200  -> checkRetcond' $ resp ^. responseBody
-         code -> addResult Critical $ "ekg endpoint returned " <> (show code ^. packed)
+        200  -> checkRetcond' $ resp ^. responseBody
+        code -> addResult Critical $ "ekg endpoint returned " <> (show code ^. packed)
   where
-    checkRetcond' bs = undefined
+    checkRetcond' bs = case (decode bs :: Maybe RetconMeters) of
+        Nothing -> addResult Critical "failed to parse ekg output"
+        Just meters -> undefined

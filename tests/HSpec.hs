f2cbab6a0f5f681f7fe20c0aa294fc190507f3e4
Sharif Olorin <sio@tesser.org>
2015-05-28 04:35:42 +0000

    Add stub test + test data

---
+++ tests/HSpec.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import           System.IO
+import           Test.Hspec
+import           Test.HUnit.Base
+
+import           System.Nagios.Plugin
+import           System.Nagios.Plugin.Retcond
+
+main :: IO ()
+main = undefined
